87b1d17da87634b905abf87adb5f4f1c5751c0fa
duenng <103260610+duenng@users.noreply.github.com>
2023-12-06 00:33:22 +0800

    added routes

--- uReplyPlus/myapp/urls.py
+++ uReplyPlus/myapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.root, name="root"),
+    path("home", views.home, name="home"),
+    path("attendance", views.attendance, name="attendance"),
+    path("qa", views.qa, name="qa"),
+    path("vote", views.vote, name="vote"),
 ]
--- uReplyPlus/myapp/views.py
+++ uReplyPlus/myapp/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def qa(request):
+    return render(request, "qa.html")
+
+def vote(request):
+    return render(request, "vote.html")
+
+def attendance(request):
+    return render(request, "attendance.html")
+
+def root(request):
+    return redirect('home')
